66f84217f1b7088f2d046065f3b1aea23fdadc6c
akshaysingh6394 <singhakshay8081@gmail.com>
2024-02-29 00:39:15 +0530

    Kadans Max Sum

--- Array/Kadans.java
+++ Array/Kadans.java
@@ -1,5 +1,36 @@
 package Array;
 
 public class Kadans {
-    
+    public static int maxSum(int arr[], int n){
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+
+            sum += arr[i];
+            if (sum > max) {
+                max = sum;
+            }
+
+            // If sum < 0: discard the sum calculated
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+
+        // To consider the sum of the empty subarray
+        // uncomment the following check:
+
+        //if (maxi < 0) maxi = 0;
+
+        return max;
+    }
+
+    public static void main(String args[]) {
+        int[] arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int n = arr.length;
+        long maxSum = maxSum(arr, n);
+        System.out.println("The maximum subarray sum is: " + maxSum);
+
+    }
+
 }
