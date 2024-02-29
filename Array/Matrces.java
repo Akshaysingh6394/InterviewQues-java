980394f709dcab01942ad1c98c06e6574644e775
Ayushi Singh <ayushisinghpatel373@gmail.com>
2024-03-01 03:54:39 +0530

    Create Matrces

---
+++ Array/Matrces.java
@@ -0,0 +1,26 @@
+package practice;
+import java.util.*;
+
+public class Matrices {
+    public static void main(String[] args) {
+        int matrix[][] = new int[3][3];
+        int n = matrix.length, m=matrix[0].length;
+      
+        Scanner sc = new Scanner(System.in);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+      
+        //output
+        for(int i=0;i<n;i++){
+            for (int j = 0; j < m; j++) {
+                System.out.println(matrix[i][j]+ " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
