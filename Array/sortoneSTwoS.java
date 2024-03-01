50626131cfde4fae4207f076b284535702f0e2fa
akshaysingh6394 <singhakshay8081@gmail.com>
2024-03-01 21:21:57 +0530

    sorting

---
+++ Array/sortoneSTwoS.java
@@ -0,0 +1,42 @@
+import java.util.*;
+public class sortoneSTwoS {
+    public static void sortArray(ArrayList<Integer> arr, int n) {
+        int low = 0, mid = 0, high = n - 1; // 3 pointers
+
+        while (mid <= high) {
+            if (arr.get(mid) == 0) {
+                // swapping arr[low] and arr[mid]
+                int temp = arr.get(low);
+                arr.set(low, arr.get(mid));
+                arr.set(mid, temp);
+
+                low++;
+                mid++;
+
+            } else if (arr.get(mid) == 1) {
+                mid++;
+
+            } else {
+                // swapping arr[mid] and arr[high]
+                int temp = arr.get(mid);
+                arr.set(mid, arr.get(high));
+                arr.set(high, temp);
+
+                high--;
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        int n = 6;
+        ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(new Integer[] {0, 2, 1, 2, 0, 1}));
+        sortArray(arr, n);
+        System.out.println("After sorting:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr.get(i) + " ");
+        }
+        System.out.println();
+
+    }
+
+}
